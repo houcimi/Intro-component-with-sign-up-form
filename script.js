718810cfd2f6a459f7b2937e28d3c1e8225b757a
houcimi <mohamed.houcimi@gmail.com>
2021-06-10 12:51:59 +0100

    mobile version finished

--- script.js
+++ script.js
@@ -3,31 +3,32 @@ const lastname = document.getElementById('lastname')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const submitBtn = document.getElementById('submot')
-const divwrapper = document.querySelectorAll('.inpt')
+const errorImg = document.querySelectorAll('#error-img')
 const errorMsg = document.querySelectorAll('#error-msg')
+const formControlls = document.querySelectorAll('.inpt')
+
 submitBtn.addEventListener('click' , (e) => {
     e.preventDefault()
     const emailel = email.value
     console.log(emailel)
     if(firstname.value == ''){
-        divwrapper[0].classList.add('active')
-        errorMsg[0].style.display = "block"
+        errorImg[0].style.opacity = 1
         firstname.classList.add('wrong')
-
+        firstname.parentNode.querySelector('span').innerText = "First Name is required"
     }
     if(lastname.value == '') {
-        divwrapper[1].classList.add('active')
-        errorMsg[1].style.display = "block"
+        errorImg[1].style.opacity = 1
         lastname.classList.add('wrong')
+        lastname.parentNode.querySelector('span').innerText = "Last Name is required"
     }
     if( email.value == '' || !emailel.includes('@')) {
-        divwrapper[2].classList.add('active')
-        errorMsg[2].style.display = "block"
+        errorImg[2].style.opacity = 1
         email.classList.add('wrong')
+        email.parentNode.querySelector('span').innerText = "This Does not look like an email"
     }
     if(password.value == '') {
-        divwrapper[3].classList.add('active')
-        errorMsg[3].style.display = "block"
+        errorImg[3].style.opacity = 1
         password.classList.add('wrong')
+        password.parentNode.querySelector('span').innerText = "Password is required"
     }
 })
